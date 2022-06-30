17e382e95bc89f3bd71f6d9c2b0eff383db9a924
Haohua Su <hsu@justworks.com>
2022-06-30 17:13:59 -0400

    modify grid border implementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 
-const getDaysInMonth = (year, month) => {
-  return new Date(year, month, 0).getDate();
-};
-
-const getPrePadDays = (year, month) => {
-  return new Date(year, month - 1, 1).getDay();
-};
-
 function App() {
-  const [current, setCurrent] = useState(new Date());
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [currentYearAndMonth, setCurrentYearAndMonth] = useState({
+    year: new Date().getFullYear(),
+    month: new Date().getMonth() + 1,
+  });
+
+  const getDaysInMonth = (year, month) => {
+    return new Date(year, month, 0).getDate();
+  };
+
+  const getPrePadDays = (year, month) => {
+    return new Date(year, month - 1, 1).getDay();
+  };
+
+  const isSameDate = (year, month, day) => {
+    return (
+      currentDate.getFullYear() === year &&
+      currentDate.getMonth() === month &&
+      currentDate.getDate() === day
+    );
+  };
 
   // days of a week
-  const weekDays = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  const dayOfWeeks = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
   // the year and month info for the previous month, current month, and the next month
-  const currentYear = current.getFullYear();
-  const currentMonth = current.getMonth() + 1;
+  const currentYear = currentYearAndMonth.year;
+  const currentMonth = currentYearAndMonth.month;
   const lastYear = currentMonth === 1 ? currentYear - 1 : currentYear;
   const nextYear = currentMonth === 12 ? currentYear + 1 : currentYear;
   const lastMonth = currentMonth === 1 ? 12 : currentMonth - 1;
@@ -25,50 +37,87 @@ function App() {
 
   // number of days in the current month
   const days = getDaysInMonth(currentYear, currentMonth);
-  const lastDays = getDaysInMonth(lastYear, lastMonth);
+  const daysOfLastMonth = getDaysInMonth(lastYear, lastMonth);
 
   // number of days to be appending before and after the current month
   const prePadDays = getPrePadDays(currentYear, currentMonth);
   const postPadDays = (7 - ((days + prePadDays) % 7)) % 7;
 
   useEffect(() => {
-    // console.log(currentYear);
-    // console.log(currentMonth);
-    // console.log(lastYear);
-    // console.log(nextYear);
-    // console.log(lastMonth);
-    // console.log(nextMonth);
-    // console.log(days);
-    // console.log(prePadDays);
-    // console.log(postPadDays);
+    console.log(currentYear);
+    console.log(currentMonth);
+    console.log(lastYear);
+    console.log(nextYear);
+    console.log(lastMonth);
+    console.log(nextMonth);
+    console.log(days);
+    console.log(prePadDays);
+    console.log(postPadDays);
+    console.log(currentYearAndMonth);
   }, []);
 
   return (
     <div className="calendar-body">
-      {[
-        weekDays.map((x, i) => (
-          <div className="week-day" key={"week-day-" + i}>
-            {x}
-          </div>
-        )),
-        [...Array(prePadDays)].map((x, i) => (
-          <div className="day" key={"pre-pad-day-" + i}>
-            <span className="span-day span-pad-day">
-              {lastDays - prePadDays + i + 1}
-            </span>
-          </div>
-        )),
-        [...Array(days)].map((x, i) => (
-          <div className="day" key={"day-" + i}>
-            <span className="span-day">{i + 1}</span>
-          </div>
-        )),
-        [...Array(postPadDays)].map((x, i) => (
-          <div className="day" key={"post-pad-day-" + i}>
-            <span className="span-day span-pad-day">{i + 1}</span>
-          </div>
-        )),
-      ]}
+      <div className="calendar-week-body">
+        {[
+          dayOfWeeks.map((day, i) => (
+            <div className="day-of-week" key={"day-of-week-" + i}>
+              {day}
+            </div>
+          )),
+        ]}
+      </div>
+      <div className="calendar-day-body">
+        {[
+          [...Array(prePadDays)].map((x, i) => (
+            <div className="day-of-month" key={"pre-pad-day-" + i}>
+              <span className="date-number pad-date-number">
+                {daysOfLastMonth - prePadDays + i + 1}
+              </span>
+            </div>
+          )),
+          [...Array(days)].map((x, i) => (
+            <div
+              className={`day-of-month ${
+                isSameDate(currentYear, currentMonth - 1, i + 1) ? "today" : ""
+              }`}
+              key={"day-" + i}
+            >
+              <span className="date-number">{i + 1}</span>
+            </div>
+          )),
+          [...Array(postPadDays)].map((x, i) => (
+            <div className="day-of-month" key={"post-pad-day-" + i}>
+              <span className="date-number pad-date-number">{i + 1}</span>
+            </div>
+          )),
+        ]}
+      </div>
+      <div>
+        <div>{`${currentYear}-${currentMonth}`}</div>
+        <button
+          onClick={() => {
+            const { year, month } = currentYearAndMonth;
+            const newYear = month === 1 ? year - 1 : year;
+            const newMonth = month === 1 ? 12 : month - 1;
+            setCurrentYearAndMonth({ year: newYear, month: newMonth });
+          }}
+        >
+          Left
+        </button>
+        <button
+          onClick={() => {
+            const { year, month } = currentYearAndMonth;
+            const newYear = month === 12 ? year + 1 : year;
+            const newMonth = month === 12 ? 1 : month + 1;
+            console.log(newYear);
+            console.log(newMonth);
+            setCurrentYearAndMonth({ year: newYear, month: newMonth });
+          }}
+        >
+          Right
+        </button>
+      </div>
     </div>
   );
 }
